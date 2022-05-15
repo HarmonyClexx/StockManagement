c7a396308ca6fff77a5660e03fda05e11b350a3c
HarmonyClexx <harmonymupindu@gmail.com>
2022-05-15 14:15:53 +0200

    Dashboard

---
+++ DashboardForm.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace StockManagement
+{
+    partial class DashboardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelId = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelCategory = new System.Windows.Forms.Label();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.labelQuantity = new System.Windows.Forms.Label();
+            this.textId = new System.Windows.Forms.TextBox();
+            this.textQuantity = new System.Windows.Forms.TextBox();
+            this.textPrice = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.textCategory = new System.Windows.Forms.TextBox();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.stockReportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.categoryModelsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.textBrand = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Location = new System.Drawing.Point(43, 30);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(44, 15);
+            this.labelId.TabIndex = 0;
+            this.labelId.Text = "Item Id";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(43, 70);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(66, 15);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Item Name";
+            // 
+            // labelCategory
+            // 
+            this.labelCategory.AutoSize = true;
+            this.labelCategory.Location = new System.Drawing.Point(43, 117);
+            this.labelCategory.Name = "labelCategory";
+            this.labelCategory.Size = new System.Drawing.Size(55, 15);
+            this.labelCategory.TabIndex = 2;
+            this.labelCategory.Text = "Category";
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.AutoSize = true;
+            this.labelPrice.Location = new System.Drawing.Point(411, 27);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(97, 15);
+            this.labelPrice.TabIndex = 3;
+            this.labelPrice.Text = "Price:                  R";
+            // 
+            // labelQuantity
+            // 
+            this.labelQuantity.AutoSize = true;
+            this.labelQuantity.Location = new System.Drawing.Point(411, 70);
+            this.labelQuantity.Name = "labelQuantity";
+            this.labelQuantity.Size = new System.Drawing.Size(53, 15);
+            this.labelQuantity.TabIndex = 4;
+            this.labelQuantity.Text = "Quantity";
+            // 
+            // textId
+            // 
+            this.textId.Location = new System.Drawing.Point(115, 22);
+            this.textId.MaxLength = 10;
+            this.textId.Name = "textId";
+            this.textId.PlaceholderText = "Unique item ID";
+            this.textId.Size = new System.Drawing.Size(168, 23);
+            this.textId.TabIndex = 6;
+            // 
+            // textQuantity
+            // 
+            this.textQuantity.Location = new System.Drawing.Point(506, 62);
+            this.textQuantity.MaxLength = 10;
+            this.textQuantity.Name = "textQuantity";
+            this.textQuantity.PlaceholderText = "Quantity of items";
+            this.textQuantity.Size = new System.Drawing.Size(168, 23);
+            this.textQuantity.TabIndex = 8;
+            // 
+            // textPrice
+            // 
+            this.textPrice.Location = new System.Drawing.Point(506, 22);
+            this.textPrice.MaxLength = 10;
+            this.textPrice.Name = "textPrice";
+            this.textPrice.PlaceholderText = "Cost of item";
+            this.textPrice.Size = new System.Drawing.Size(168, 23);
+            this.textPrice.TabIndex = 9;
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(115, 62);
+            this.textName.Name = "textName";
+            this.textName.PlaceholderText = "Item name";
+            this.textName.Size = new System.Drawing.Size(168, 23);
+            this.textName.TabIndex = 10;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(12, 204);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 12;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBrand);
+            this.panel1.Controls.Add(this.buttonClear);
+            this.panel1.Controls.Add(this.textCategory);
+            this.panel1.Controls.Add(this.textQuantity);
+            this.panel1.Controls.Add(this.labelId);
+            this.panel1.Controls.Add(this.labelName);
+            this.panel1.Controls.Add(this.textName);
+            this.panel1.Controls.Add(this.labelCategory);
+            this.panel1.Controls.Add(this.textPrice);
+            this.panel1.Controls.Add(this.labelPrice);
+            this.panel1.Controls.Add(this.labelQuantity);
+            this.panel1.Controls.Add(this.textId);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(934, 159);
+            this.panel1.TabIndex = 13;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(847, 117);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 13;
+            this.buttonClear.Text = "Clear Fields";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // textCategory
+            // 
+            this.textCategory.Location = new System.Drawing.Point(115, 109);
+            this.textCategory.Name = "textCategory";
+            this.textCategory.PlaceholderText = "Item category";
+            this.textCategory.Size = new System.Drawing.Size(168, 23);
+            this.textCategory.TabIndex = 12;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(153, 204);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 14;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(493, 204);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 15;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(301, 204);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 17;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 277);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(934, 284);
+            this.dataGridView1.TabIndex = 20;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stockReportToolStripMenuItem,
+            this.categoryModelsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(934, 24);
+            this.menuStrip1.TabIndex = 21;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // stockReportToolStripMenuItem
+            // 
+            this.stockReportToolStripMenuItem.Name = "stockReportToolStripMenuItem";
+            this.stockReportToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.stockReportToolStripMenuItem.Text = "Stock Report";
+            this.stockReportToolStripMenuItem.Click += new System.EventHandler(this.stockReportToolStripMenuItem_Click);
+            // 
+            // categoryModelsToolStripMenuItem
+            // 
+            this.categoryModelsToolStripMenuItem.Name = "categoryModelsToolStripMenuItem";
+            this.categoryModelsToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
+            this.categoryModelsToolStripMenuItem.Text = "Category & models";
+            this.categoryModelsToolStripMenuItem.Click += new System.EventHandler(this.categoryModelsToolStripMenuItem_Click);
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Location = new System.Drawing.Point(678, 204);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(222, 23);
+            this.buttonShow.TabIndex = 22;
+            this.buttonShow.Text = "Show all data";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // textBrand
+            // 
+            this.textBrand.Location = new System.Drawing.Point(506, 107);
+            this.textBrand.Name = "textBrand";
+            this.textBrand.PlaceholderText = "Item brand";
+            this.textBrand.Size = new System.Drawing.Size(168, 23);
+            this.textBrand.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(411, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Brand";
+            // 
+            // DashboardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(934, 561);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "DashboardForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "dashboard";
+            this.Load += new System.EventHandler(this.DashboardForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label labelId;
+        private Label labelName;
+        private Label labelCategory;
+        private Label labelPrice;
+        private Label labelQuantity;
+        private TextBox textId;
+        private TextBox textQuantity;
+        private TextBox textPrice;
+        private TextBox textName;
+        private Button buttonAdd;
+        private Panel panel1;
+        private Button buttonUpdate;
+        private Button buttonClose;
+        private Button buttonDelete;
+        private DataGridView dataGridView1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem stockReportToolStripMenuItem;
+        private TextBox textCategory;
+        private Button buttonClear;
+        private Button buttonShow;
+        private ToolStripMenuItem categoryModelsToolStripMenuItem;
+        private Label label1;
+        private TextBox textBrand;
+    }
+}
