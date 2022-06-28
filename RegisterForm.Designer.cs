e89fff796d2db0e144e545ba2b286b113fbc2bbd
HarmonyClexx <harmonymupindu@gmail.com>
2022-06-28 20:54:00 +0200

    Do register

---
+++ RegisterForm.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace StockManagement
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textUsername = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.textConfirmPassword = new System.Windows.Forms.TextBox();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.textPhone = new System.Windows.Forms.TextBox();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::StockManagement.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(70, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 170);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(112, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "GET STARTED";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::StockManagement.Properties.Resources.profile_icon;
+            this.pictureBox2.Location = new System.Drawing.Point(24, 227);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::StockManagement.Properties.Resources.lock_icon;
+            this.pictureBox3.Location = new System.Drawing.Point(24, 445);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(24, 532);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 2);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(24, 487);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(300, 2);
+            this.panel2.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(24, 268);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(300, 2);
+            this.panel3.TabIndex = 5;
+            // 
+            // textUsername
+            // 
+            this.textUsername.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textUsername.Location = new System.Drawing.Point(68, 248);
+            this.textUsername.Name = "textUsername";
+            this.textUsername.PlaceholderText = "Username";
+            this.textUsername.Size = new System.Drawing.Size(254, 16);
+            this.textUsername.TabIndex = 6;
+            // 
+            // textPassword
+            // 
+            this.textPassword.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textPassword.Location = new System.Drawing.Point(68, 467);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.PlaceholderText = "Password";
+            this.textPassword.Size = new System.Drawing.Size(254, 16);
+            this.textPassword.TabIndex = 7;
+            // 
+            // textConfirmPassword
+            // 
+            this.textConfirmPassword.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textConfirmPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textConfirmPassword.Location = new System.Drawing.Point(68, 512);
+            this.textConfirmPassword.Name = "textConfirmPassword";
+            this.textConfirmPassword.PlaceholderText = "Confirm Password";
+            this.textConfirmPassword.Size = new System.Drawing.Size(254, 16);
+            this.textConfirmPassword.TabIndex = 8;
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonRegister.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonRegister.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buttonRegister.Location = new System.Drawing.Point(24, 551);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(305, 40);
+            this.buttonRegister.TabIndex = 9;
+            this.buttonRegister.Text = "REGISTER";
+            this.buttonRegister.UseVisualStyleBackColor = false;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonClear.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buttonClear.Location = new System.Drawing.Point(24, 643);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 11;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonExit.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buttonExit.Location = new System.Drawing.Point(247, 643);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 13;
+            this.buttonExit.Text = "exit";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(85, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 15);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Group E Stock management";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(125, 601);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(204, 15);
+            this.linkLabel1.TabIndex = 15;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Already have an account? Log in here";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // textName
+            // 
+            this.textName.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textName.Location = new System.Drawing.Point(68, 311);
+            this.textName.Name = "textName";
+            this.textName.PlaceholderText = "Full Name";
+            this.textName.Size = new System.Drawing.Size(254, 16);
+            this.textName.TabIndex = 19;
+            // 
+            // panel5
+            // 
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Location = new System.Drawing.Point(24, 331);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(300, 2);
+            this.panel5.TabIndex = 18;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::StockManagement.Properties.Resources.profile_icon;
+            this.pictureBox4.Location = new System.Drawing.Point(23, 289);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 20;
+            this.pictureBox4.TabStop = false;
+            // 
+            // textEmail
+            // 
+            this.textEmail.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textEmail.Location = new System.Drawing.Point(70, 360);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.PlaceholderText = "Confirm Email";
+            this.textEmail.Size = new System.Drawing.Size(254, 16);
+            this.textEmail.TabIndex = 22;
+            // 
+            // panel6
+            // 
+            this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel6.Location = new System.Drawing.Point(26, 380);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(300, 2);
+            this.panel6.TabIndex = 21;
+            // 
+            // textPhone
+            // 
+            this.textPhone.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textPhone.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textPhone.Location = new System.Drawing.Point(68, 419);
+            this.textPhone.Name = "textPhone";
+            this.textPhone.PlaceholderText = "Confirm Phone Number";
+            this.textPhone.Size = new System.Drawing.Size(254, 16);
+            this.textPhone.TabIndex = 24;
+            // 
+            // panel7
+            // 
+            this.panel7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel7.Location = new System.Drawing.Point(24, 439);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(300, 2);
+            this.panel7.TabIndex = 23;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::StockManagement.Properties.Resources.email;
+            this.pictureBox5.Location = new System.Drawing.Point(24, 341);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 25;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::StockManagement.Properties.Resources.phone;
+            this.pictureBox6.Location = new System.Drawing.Point(24, 396);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 26;
+            this.pictureBox6.TabStop = false;
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(354, 681);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.textPhone);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.textConfirmPassword);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textUsername);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "RegisterForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RegisterForm";
+            this.Load += new System.EventHandler(this.RegisterForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private TextBox textUsername;
+        private TextBox textPassword;
+        private TextBox textConfirmPassword;
+        private Button buttonRegister;
+        private Button buttonClear;
+        private Button buttonExit;
+        private Label label2;
+        private LinkLabel linkLabel1;
+        private TextBox textBox1;
+        private Panel panel4;
+        private TextBox textBox2;
+        private Panel panel5;
+        private PictureBox pictureBox4;
+        private TextBox textBox3;
+        private Panel panel6;
+        private TextBox textBox4;
+        private Panel panel7;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private TextBox textEmail;
+        private TextBox textPhone;
+        private TextBox textName;
+    }
+}
---
+++ RegisterForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+using System.IO;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Mail;
+
+namespace StockManagement
+{
+    public partial class RegisterForm : Form
+    {
+        string path = "Data Source=DESKTOP-APNN3FD\\SQLEXPRESS;Initial Catalog=StockManagement;Integrated Security=True";
+        SqlConnection con;
+        SqlCommand cmd;
+
+        public RegisterForm()
+        {
+            InitializeComponent();
+            con = new SqlConnection(path);
+        }
+
+        private void RegisterForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            if (textUsername.Text == "" || textPassword.Text=="" || textConfirmPassword.Text=="" || textEmail.Text=="" || textPhone.Text=="" || textName.Text=="")
+            {
+                MessageBox.Show("All the fields should be filled");
+            }
+            else
+            {
+                if (textPassword.Text == textConfirmPassword.Text)
+                {
+                    con.Open();
+                    cmd = new SqlCommand("insert into gelogin (Username, FullName, Password, Email, Phone) values('"+ textUsername.Text + "', '" + textName.Text + "', '"+ textPassword.Text + "', '"+ textEmail.Text + "', '"+ textPhone.Text + "')", con);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    //Use streamWriter if you dont have a database
+                    /* using (StreamWriter streamWriter = new StreamWriter("loginDetails.txt"))
+                     {
+                         streamWriter.WriteLine(username);
+                         streamWriter.WriteLine(password);
+                     }*/
+
+                    MessageBox.Show("Your login details have been saved, now LOG IN");
+                    new LoginForm().Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("the passwords do not match, try again");
+
+                    textPassword.Clear();
+                    textConfirmPassword.Clear();
+                }
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textUsername.Clear();
+            textPassword.Clear();
+            textConfirmPassword.Clear();
+            textUsername.Focus();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            new LoginForm().Show();
+            this.Hide();
+        }
+    }
+}
