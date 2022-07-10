4f955159c71a282a697e37a8938ca39c92f9b347
HarmonyClexx <harmonymupindu@gmail.com>
2022-07-10 21:04:00 +0200

    Report

---
+++ ReportForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace StockManagement
+{
+    partial class ReportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textSearch = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.backToHomeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textSearch
+            // 
+            this.textSearch.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textSearch.Location = new System.Drawing.Point(385, 61);
+            this.textSearch.Name = "textSearch";
+            this.textSearch.PlaceholderText = "Search Item here";
+            this.textSearch.Size = new System.Drawing.Size(537, 23);
+            this.textSearch.TabIndex = 0;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(280, 61);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 1;
+            this.buttonSearch.Text = "Search:";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 125);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(910, 424);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(311, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "The whole Stock Report";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backToHomeToolStripMenuItem,
+            this.changeViewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(934, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // backToHomeToolStripMenuItem
+            // 
+            this.backToHomeToolStripMenuItem.Name = "backToHomeToolStripMenuItem";
+            this.backToHomeToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.backToHomeToolStripMenuItem.Text = "Back to Home";
+            this.backToHomeToolStripMenuItem.Click += new System.EventHandler(this.backToHomeToolStripMenuItem_Click);
+            // 
+            // changeViewToolStripMenuItem
+            // 
+            this.changeViewToolStripMenuItem.Name = "changeViewToolStripMenuItem";
+            this.changeViewToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
+            this.changeViewToolStripMenuItem.Text = "Category & models";
+            this.changeViewToolStripMenuItem.Click += new System.EventHandler(this.changeViewToolStripMenuItem_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.BackgroundImage = global::StockManagement.Properties.Resources.icons8_refresh_24;
+            this.buttonRefresh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRefresh.Location = new System.Drawing.Point(358, 60);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(28, 27);
+            this.buttonRefresh.TabIndex = 6;
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // ReportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(934, 561);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textSearch);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ReportForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textSearch;
+        private Button buttonSearch;
+        private DataGridView dataGridView1;
+        private Label label1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem backToHomeToolStripMenuItem;
+        private Button buttonRefresh;
+        private ToolStripMenuItem changeViewToolStripMenuItem;
+    }
+}
---
+++ ReportForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace StockManagement
+{
+    public partial class ReportForm : Form
+    {
+        string path = "Data Source=DESKTOP-APNN3FD\\SQLEXPRESS;Initial Catalog=StockManagement;Integrated Security=True";
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        DataTable dt;
+
+        public ReportForm()
+        {
+            InitializeComponent();
+            con = new SqlConnection(path);
+            DisplayData();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            //var updateQuery = "";
+            dt = new DataTable();
+
+            if (textSearch.Text == "")
+            {
+                MessageBox.Show("please type what you want to search!");
+            }
+            else
+            {
+                
+
+                if (AlreadyExist(con, textSearch.Text))
+                {
+                    sda = new SqlDataAdapter("SELECT * FROM Stocks WHERE Category= '" + textSearch.Text + "'", con);
+                    sda.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                    
+
+                }
+                else
+                {
+                    MessageBox.Show("Item name DOES NOT exist\n You can go to home window and add the Record");
+                }
+               
+            }
+            con.Close();
+        }
+
+        public void DisplayData()
+        {
+            dt = new DataTable();
+            con.Open();
+            sda = new SqlDataAdapter("SELECT * FROM Stocks", con);
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void backToHomeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new DashboardForm().Show();
+            this.Hide();
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            DisplayData();
+        }
+
+        private bool AlreadyExist(SqlConnection con, string category)
+        {
+            sda = new SqlDataAdapter("select 1 from Stocks WHERE Category= '" + category + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private void changeViewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new DisplayView().Show();
+            this.Hide();
+        }
+    }
+}
